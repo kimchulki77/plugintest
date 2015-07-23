b2c54b331413a02b7a40838e24d63df7b55cfa76
kimchulki77 <kimchulki77@naver.com>
2015-07-23 11:41:44 +0900

    ㅇ

---
+++ platforms.js
@@ -0,0 +1,16 @@
+//This script will add or remove all plugins listed in package.json
+//usage: node platforms.js [add | remove]
+
+var command = process.argv[2] || 'add';
+var packageJson = require('./package.json');
+
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+packageJson.platforms.forEach(function(platform) {
+    var platformCmd = 'cordova platform ' + command + ' ' + platform;
+console.log(platformCmd);
+    exec(platformCmd);
+});
